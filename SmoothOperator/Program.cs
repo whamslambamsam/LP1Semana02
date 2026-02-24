0a886ce782e66904076f3cdfeb60ddee983ea5f3
Samuel Esteves <ssamuelesteves13@gmail.com>
2026-02-24 17:30:21 +0000

    Completed Exercise 1

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace SmoothOperator
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Insere número inteiro: ");
+            sbyte n = Convert.ToSByte(Console.ReadLine());
+            Console.WriteLine(--n);
+            Console.WriteLine(++n);
+        }
+    }
+}
