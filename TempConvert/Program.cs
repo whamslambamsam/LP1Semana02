17b66371664754f6b2f67d7fed107e9dd2bb9006
Samuel Esteves <ssamuelesteves13@gmail.com>
2026-02-26 16:06:44 +0000

    Created TempConvert project

---
+++ TempConvert/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TempConvert
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP!");
+        }
+    }
+}
