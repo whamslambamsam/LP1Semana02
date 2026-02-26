90a791db1954356b11f9e38092cb39d04444c82b
Samuel Esteves <ssamuelesteves13@gmail.com>
2026-02-26 15:33:47 +0000

    Added altura and raio variables

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -6,6 +6,11 @@ namespace Cilindro
     {
         private static void Main(string[] args)
         {
+            Console.Write("Insira altura: ");
+            double altura = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Insira raio: ");
+            double raio = Convert.ToDouble(Console.ReadLine());
         }
     }
 }
