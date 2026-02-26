4e3c81ee764288d3a34d9af74f1165e97f6933fa
Samuel Esteves <ssamuelesteves13@gmail.com>
2026-02-26 16:02:49 +0000

    Created ValoresEspeciais project

---
+++ ValoresEspeciais/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ValoresEspeciais
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Double: ");
+            Console.WriteLine(double.MaxValue);
+            Console.WriteLine(double.MinValue);
+
+            Console.WriteLine("");
+
+            Console.WriteLine("SByte: ");
+            Console.WriteLine(sbyte.MaxValue);
+            Console.WriteLine(sbyte.MinValue);
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Byte: ");
+            Console.WriteLine(byte.MaxValue);
+            Console.WriteLine(byte.MinValue);
+
+            Console.WriteLine("");
+
+            Console.WriteLine("ULong: ");
+            Console.WriteLine(ulong.MaxValue);
+            Console.WriteLine(ulong.MinValue);
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Float: ");
+            Console.WriteLine(float.MaxValue);
+            Console.WriteLine(float.MinValue);
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Integer: ");
+            Console.WriteLine(int.MaxValue);
+            Console.WriteLine(int.MinValue);
+        }
+    }
+}
