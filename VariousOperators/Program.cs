ff778a07d294e8d71890deb7c6beff86ceb499d7
Samuel Esteves <ssamuelesteves13@gmail.com>
2026-02-26 15:21:41 +0000

    Completed Exercise 4

--- VariousOperators/Program.cs
+++ VariousOperators/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
 
 namespace VariousOperators
 {
@@ -6,7 +9,22 @@ namespace VariousOperators
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP!");
+            Console.Write("Insere número inteiro-não negativo: ");
+            byte n = Convert.ToByte(Console.ReadLine());
+            bool isBigger = false;
+            
+            Console.WriteLine(n / 2);
+            Console.WriteLine(n << 3);
+            Console.WriteLine(n ^ 6);
+            if (n > 10)
+            {
+                isBigger = true;
+                Console.WriteLine(isBigger);
+            }
+            else
+            {
+                Console.WriteLine(isBigger);
+            }
         }
     }
 }
