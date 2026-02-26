0ebb9c6bc5b871df0914448012ae06e4205de28d
Samuel Esteves <ssamuelesteves13@gmail.com>
2026-02-26 15:45:52 +0000

    Made it so values get rounded

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.Intrinsics.X86;
 
 namespace Cilindro
 {
@@ -13,10 +14,10 @@ namespace Cilindro
             double raio = Convert.ToDouble(Console.ReadLine());
 
             double volume = Math.PI * Math.Pow(raio, 2) * altura;
-            Console.WriteLine(volume); 
+            Console.WriteLine(Math.Round(volume, 3));
 
             double areaSuperficie = 2 * Math.PI * raio * (raio + altura);
-            Console.WriteLine(areaSuperficie);
+            Console.WriteLine(Math.Round(areaSuperficie, 3));
         }
     }
 }
